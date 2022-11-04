95dad77731dffa5e19da47209976da0be4051d6e
Jerolussich <jerolussich@hotmail.com>
2022-11-04 12:14:10 -0700

    fixes to functions

--- functions.c
+++ functions.c
@@ -2,31 +2,36 @@
 #include "main.h"
 int printstring (va_list args)
 {
-	char i = va_arg(args, int);
+	char *i = va_arg(args, char *);
+	int j = 0;
 
-	while (i)
+	while (i[j])
 	{
-		putchar(args[i]);
-		i++;
+		putchar(i[j]);
+		j++;
 	}
 	return (0);
 }
-
 int printnumber (va_list args)
 {
-	int i = va_arg(va_list int);
+	int i = va_arg(args, int);
 
 	if (i < 0)
 	{
 		_putchar('-');
 		i = -i;
-}
-int putnumber (va_list args)
-{
-	if (i / 10)
-	{
-		putnumber (i /10);
-		_putchar (i % 10 + '0');
 	}
-	return (i);
+	else
+		putnumber(i);
+
+	return (0);
+}
+int putnumber (int number)
+{
+	if ((number / 10) > 0)
+	{
+		putnumber(number / 10);
+		_putchar (number % 10 + '0');
+	}
+	return (number);
 }
